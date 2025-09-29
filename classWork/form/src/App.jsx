69149d3b782f30949c67a1a6234d9e47ec7d8496
Likhil N Maiya <134200383+Lickhill@users.noreply.github.com>
2025-09-29 09:48:28 +0530

    use context and useeffect

--- classWork/form/src/App.jsx
+++ classWork/form/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Form from "./components/form";
+import Usecontextstudy from "./components/usecontextstudy";
+import Useeffectstudy from "./components/useeffectstudy";
 
 function App() {
 	return (
 		<div className="App">
 			<Form />
+			<Usecontextstudy />
+			<Useeffectstudy />
 		</div>
 	);
 }
